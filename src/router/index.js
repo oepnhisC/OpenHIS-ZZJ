92b61a2045d4b92a24cbc338d65d8e7f1d12e0e6
openhisC <openhisauthor@outlook.com>
2024-11-24 09:06:52 +0800

    删除无用页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import DianZiYiBao from '@/views/DianZiYiBao.vue'
 import PayDetails from '@/views/PayDetails.vue'
 import DanJu from '@/views/DanJu.vue'
 import WeiXinPay from '@/views/WeiXinPay.vue';
@@ -12,7 +11,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {      path: '/',      name: 'home',      component: HomeView    },
-    {      path: '/dianziyibao',      name: 'dianziyibao',      component: DianZiYiBao    },
     {      path: '/paydetails',      name: 'paydetails',      component: PayDetails    },
     {      path: '/danju',      name: 'danju',      component: DanJu    },
     {      path: '/weixinpay',      name: 'weixinpay',      component: WeiXinPay    },
